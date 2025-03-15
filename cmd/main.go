987a559b2328010c0aa73b3ca0676a0ab35d5453
porthos <alirezaportous@gmail.com>
2025-03-15 13:15:37 +0330

    Add routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"hexagonal_video_streaming/internal/adapters/handlers"
+	"hexagonal_video_streaming/internal/adapters/repository"
+	"hexagonal_video_streaming/internal/core/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	videoRepo := repository.New("../videos")
+	videoService := service.New(videoRepo)
+	videoHandler := handlers.New(videoService)
+
+	r := gin.Default()
+	r.GET("/stream/:filename", videoHandler.StreamVideo)
+
+	r.Run(":8080")
+}
